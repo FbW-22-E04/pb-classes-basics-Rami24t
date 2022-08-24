12cd409fb3bc55b14aef7037c5dc495517298da9
Rami <103028944+Rami24t@users.noreply.github.com>
2022-08-24 16:19:32 +0200

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,12 @@
+//1. a date object , instance of Date . 
+//2. Date Object 
+//3. Date is a Class 
+//4. Function
+//5. A Date object
+//6. A Funciton
+//7. A Date object
+//8. yes
+//9. false. because they are not equal dates and they are objects
+// 10. false
+// 11. false
+// 12. false , they are different objects.
